067b2fc3ae2c0ee918813c2fe35c6bb494b7640b
huihui43 <506057576@qq.com>
2020-01-10 10:25:20 +0800

    update

---
+++ textonboost.py
@@ -0,0 +1,45 @@
+'''
+implement texton data and textonboost classifier
+
+'''
+
+class textonData(object):
+    def __init__(self,i,j):
+        """initialize some variables
+        
+        Arguments:
+            object {[type]} -- [description]
+            i {int} -- [image pixel id]
+            j {int} -- [image row id]
+        """
+        self.x_ =  j
+        self.y_ = i
+
+        
+    def evalInt(self,x1,y1,x2,y2,t):
+        """computer 
+        
+        Arguments:
+            x1 {[type]} -- [description]
+            y1 {[type]} -- [description]
+            x2 {[type]} -- [description]
+            y2 {[type]} -- [description]
+            t {[type]} -- [description]
+        """
+        pass
+
+
+class joinBoost(object):
+    pass
+
+
+class textonboost(object):
+    """class object for textonboost calssifier
+    
+    Arguments:
+        object {[type]} -- [description]
+    """
+    def __init__(object):
+        pass
+
+    def random_pick
